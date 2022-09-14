6c57dd9b3e4880878fd0f29be14872f6b372479d
KarynaMisnik <96831988+KarynaMisnik@users.noreply.github.com>
2022-09-14 17:26:57 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Game game = new Game();
+
+	}
+
+	
+	public Main() {
+		
+	}
+}
